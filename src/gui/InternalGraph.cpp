04157a8846e7049370d3567db19724964672083c
Charles Giessen <cdgiessen@gmail.com>
2018-04-01 19:02:57 -0500

    formatting

--- src/gui/InternalGraph.cpp
+++ src/gui/InternalGraph.cpp
@@ -5,24 +5,6 @@
 
 namespace InternalGraph {
 
-	inline float
-	BilinearInterpolation(float q11, float q12, float q21, float q22, float x1, float x2, float y1, float y2, float x, float y)
-	{
-		float x2x1, y2y1, x2x, y2y, yy1, xx1;
-		x2x1 = x2 - x1;
-		y2y1 = y2 - y1;
-		x2x = x2 - x;
-		y2y = y2 - y;
-		yy1 = y - y1;
-		xx1 = x - x1;
-		return 1.0 / (x2x1 * y2y1) * (
-			q11 * x2x * y2y +
-			q21 * xx1 * y2y +
-			q12 * x2x * yy1 +
-			q22 * xx1 * yy1
-			);
-	}
-
 	template<typename T>
 	static float BilinearImageSample2D(const NoiseImage2D<T>& noiseImage, const float x, const float z) {
 		int cellScale = noiseImage.GetImageWidth() - 1; // cellsWide - 1;
@@ -40,8 +22,6 @@ namespace InternalGraph {
 		float UR = noiseImage.BoundedLookUp(realX, realZPlus1);
 		float DL = noiseImage.BoundedLookUp(realXPlus1, realZ);
 		float DR = noiseImage.BoundedLookUp(realXPlus1, realZPlus1);
-
-		//return BilinearInterpolation(UL, UR, DL, DR, realX, realXPlus1, realZ, realZPlus1, x, z);
 
 		if (realX == realXPlus1 && realZ == realZPlus1) {
 			return UL;
